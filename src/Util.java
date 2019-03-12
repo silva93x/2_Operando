8d0e51b572eac3046f40a04b94e52d590d29588d
Lucía <luciamonterog24@gmail.com>
2019-03-12 12:06:33 +0100

    método sumar

--- src/Util.java
+++ src/Util.java
@@ -3,7 +3,18 @@ public class Util {
 
 	public static void main(String[] args) {
 		
+		int a = 3;
+		int b = 4;
+		
 		System.out.println("Vamos a operar");
+		
+		sumar(a, b);
+		
+	}
+	
+	public static void sumar(int a, int b) {
+		
+		System.out.println(a+"+"+b+"="+(a+b));
 		
 	}
 	
